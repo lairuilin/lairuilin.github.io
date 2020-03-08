713a26791ce635922b9e267092ead7e1f946a2ff
lairuilin <kalai850531@g.ncu.edu.tw>
2020-03-08 23:18:37 +0800

    汶葶碎碎念

---
+++ teacher/test.php
@@ -0,0 +1,176 @@
+<html>
+
+<header>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<link rel="stylesheet" type="text/css" href="start.css">
+
+</header>
+
+
+<body>
+  <img class="chanlablogo"src="chanlablogo.png" alt="logo" style="width: 600px;">
+    
+<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="true">個人簡介</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-job-tab" data-toggle="pill" href="#pills-job" role="tab" aria-controls="pills-job" aria-selected="false">職位</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-school-tab" data-toggle="pill" href="#pills-school" role="tab" aria-controls="pills-school" aria-selected="false">學歷</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-study-tab" data-toggle="pill" href="#pills-study" role="tab" aria-controls="pills-study" aria-selected="false">研究興趣</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-service-tab" data-toggle="pill" href="#pills-service" role="tab" aria-controls="pills-service" aria-selected="false">學術服務</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-project-tab" data-toggle="pill" href="#pills-project" role="tab" aria-controls="pills-project" aria-selected="false">研究計畫</a>
+  </li>
+   <li class="nav-item">
+    <a class="nav-link" id="pills-class-tab" data-toggle="pill" href="#pills-class" role="tab" aria-controls="pills-class" aria-selected="false">開設課程</a>
+  </li>
+   <li class="nav-item">
+    <a class="nav-link" id="pills-award-tab" data-toggle="pill" href="#pills-award" role="tab" aria-controls="pills-award" aria-selected="false">得獎</a>
+  </li>
+   <li class="nav-item">
+    <a class="nav-link" id="pills-interview-tab" data-toggle="pill" href="#pills-interview" role="tab" aria-controls="pills-interview" aria-selected="false">專訪</a>
+  </li>
+   <li class="nav-item">
+    <a class="nav-link" id="pills-patent-tab" data-toggle="pill" href="#pills-patent" role="tab" aria-controls="pills-patent" aria-selected="false">專利</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-paper-tab" data-toggle="pill" href="#pills-paper" role="tab" aria-controls="pills-paper" aria-selected="false">學術著作</a>
+  </li>
+   <li class="nav-item">
+    <a class="nav-link" id="pills-chpaper-tab" data-toggle="pill" href="#pills-chpaper" role="tab" aria-controls="pills-chpaper" aria-selected="false">中文著作</a>
+  </li>
+   <li class="nav-item">
+    <a class="nav-link" id="pills-student-tab" data-toggle="pill" href="#pills-student" role="tab" aria-controls="pills-student" aria-selected="false">畢業學生</a>
+  </li>
+   <li class="nav-item">
+    <a class="nav-link" id="pills-speech-tab" data-toggle="pill" href="#pills-speech" role="tab" aria-controls="pills-speech" aria-selected="false">學術演講</a>
+  </li>
+   <li class="nav-item">
+    <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="#pills-contact" role="tab" aria-controls="pills-contact" aria-selected="false">聯絡方式</a>
+  </li>
+   <li class="nav-item">
+    <a class="nav-link" id="pills-chancycle-tab" data-toggle="pill" href="#pills-chancycle" role="tab" aria-controls="pills-chancycle" aria-selected="false">德懷家族</a>
+  </li>
+</ul>
+
+
+<div class="tab-content" id="pills-tabContent">
+
+  <div class="tab-pane fade show active" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+    <div class="content">個人簡介資料
+    </div>
+  </div>
+
+  <div class="tab-pane fade" id="pills-job" role="tabpanel" aria-labelledby="pills-job-tab">
+    <div class="content">
+		陳德懷教授現任學網路學習科技研究所講座教授。他在香港出生、長大。負笈英國取得大學學位後，在美國取得碩士和博士學位。之後一直在台任教。然而，陳教授家人皆已移民加拿大(除了他之外)，並持有加拿大公民身分。為了證明自己是「真正」的全球華人，他幽默的表示未來可能會到非洲退休。
+
+數位學習是一個典型科技與人文結合的領域，此領域包含學習軟體技術、教育理論、認知心理、以及網路與人機介面等理論知識與技術。目前數位學習領域是一個新興且急速發展的研究，也是未來教育改革最大的動力。台灣的數位學習研究目前備受歐美各國重視，且領先於亞洲。陳教授正是台灣這研究領域的主要奠基者之一。
+
+ 
+
+過去二十五年來，陳教授致力於研究數位學習多個子領域，包括人工智慧在教育的應用、電腦輔助合作學習、數位教室、網路學習社群、行動與無所不在學習。最近他則致力於以數位遊戲為基礎的學習，研究如何使用數位科技增強小學的數學及語文學習。
+
+陳教授曾主持過經費近五億台幣「大學學術追求卓越研究計畫」，這個計畫有三十多位教授參與。其中研發的「亞卓市」的學習平台，帶起國內數位學習熱潮，他也曾協助劉兆漢院士規劃並推動第一期「國家型數位學習科技計畫」。陳教授的貢獻大幅提升了台灣數位學習的研究能量。
+
+二十多年前陳教授在念博士的時候，數位學習是個冷門的題材。在當時電子計算機系所中，少有教授研究教育應用，那時還是主修電子計算機博士生的他，最後克服困難，在博士論文提出模擬「學習同伴」的觀念，應用在人工智慧教育應用領域上，開?了一個新的子領域，並發展數位社會性學習理論。
+
+1989年取得博士後，陳教授即在中央大學任教。來台後的第一項研究，發展了世界第一套專門為網路學習而設計的系統原型，並進行社會性學習的研究。於2000年至2004年間陳教授主持「學習科技 –– 主動社會學習及其應用，從台灣到全世界」的卓越研究計畫。此計畫中所建構的「亞卓市」，是使用後來被稱為的Web2.0技術，且亞卓市是本世紀初全球規模最大的網路學習社群網站，有超過一半台灣的中小學在亞卓市建構網站。自從亞卓市成立之後，與亞卓市架構類似的一些大型整合性網路學習平台才在歐亞地區相繼出現，例如香港後來就有一個「香港教育城」。最近亞卓市也成為美國「歐巴馬政府數位學習國家計畫」的國際參考例子。
+
+除了「亞卓市」之外，卓越計畫中有行動學習教室科技(即俗稱「電子書包」，同一時間朱邦復先生也推廣相同觀念) 的研究，引發他在2003年帶頭與一批歐美學者提倡「一對一數位學習」，即每位學生都有一台無線數位裝置進行學習的研究。陳教授在學界對整個理念的推動，比MIT提倡的「一童一機」還早了幾年。所以陳教授的大力推動造成電子書包和一對一數位學習這些觀念持續的發酵，相信後來才觸發了Intel研發Classmate、台灣華碩公司研發了 Eee PC以及宏碁推出的低價電腦。
+
+在2003年，陳教授以卓越研究計畫的成果作為基礎，協助劉兆漢院士規劃第一期「國家型數位學習科技計畫」，並在2007年協助規劃第二期的「國家型數位典藏與學習科技計畫」中有關前瞻技術研究的部份。由ISI關於國際期刊資料，在六種SSCI或SCI數位學習期刊中，台灣近五年在論文數和衝擊指數(impact factor)方面，全世界排名第三，僅次於美國和英國。在全球、亞洲以及全球華人在數位學習領域研究社群裡，陳教授極具廣泛影響力。他常被邀請到國際頂尖學術研討會演講，並曾擔任十多種國際期刊的編輯，創立三個數位學習組織，長期投入建構國內、全球華人，以及亞太地區數位學習研究社群。
+
+數位學習產業是國內規模尚小，但成長快速的新興產業。根據IDC的資料顯示，國內數位學習產業的複合成長率超過60%，比全球、北美和亞洲不足30%的成長率高出許多。多年前發展的教室無線回饋反應系統，是中央大學第一個技術轉移案子，並在1999年促成一間公司的成立，該產品在國內外多所大學和中小學使用。此外，亞卓市自研究計畫結束之後，成功轉移給中華電信公司繼續深化經營，使用人口仍持續增加。
+
+在全球、亞洲以及全球華人數位學習領域研究社群裡，陳教授具有廣泛影響力。他曾擔任十多種國際期刊的編輯，創立三個數位學習組織，並常被邀請到國際頂尖學術研討會演講，例如，2007年在一個國際會議上的演講，陳教授提出未來一百年研究者需要解決的四個問題，都得到廣泛的迴響。又如今年在數位學習重要期刊JCAL的廿五週年特刊，他被邀請發表position paper，此論文描述未來二十年教室因為學習科技可能經歷的改變過程。
+    </div>
+  </div>
+
+  <div class="tab-pane fade" id="pills-school" role="tabpanel" aria-labelledby="pills-school-tab">
+    <div class="content">3
+    </div>
+  </div>
+
+  <div class="tab-pane fade" id="pills-study" role="tabpanel" aria-labelledby="pills-study-tab">
+    <div class="content">4
+    </div>
+  </div>
+
+  <div class="tab-pane fade" id="pills-service" role="tabpanel" aria-labelledby="pills-service-tab">
+    <div class="content">5
+    </div>
+  </div>
+
+  <div class="tab-pane fade" id="pills-project" role="tabpanel" aria-labelledby="pills-project-tab">
+    <div class="content">6
+    </div>
+  </div>
+
+  <div class="tab-pane fade" id="pills-class" role="tabpanel" aria-labelledby="pills-class-tab">
+    <div class="content">7
+    </div>
+  </div>
+
+  <div class="tab-pane fade" id="pills-award" role="tabpanel" aria-labelledby="pills-award-tab">
+    <div class="content">8
+    </div>  
+  </div>
+
+  <div class="tab-pane fade" id="pills-interview" role="tabpanel" aria-labelledby="pills-interview-tab">
+    <div class="content">9
+    </div>
+  </div>
+
+  <div class="tab-pane fade" id="pills-patent" role="tabpanel" aria-labelledby="pills-patent-tab">
+    <div class="content">10
+    </div>
+  </div>
+
+  <div class="tab-pane fade" id="pills-paper" role="tabpanel" aria-labelledby="pills-paper-tab">
+    <div class="content">11
+    </div>
+  </div>
+
+  <div class="tab-pane fade" id="pills-chpaper" role="tabpanel" aria-labelledby="pills-chpaper-tab">
+    <div class="content">12
+    </div>
+  </div>
+
+  <div class="tab-pane fade" id="pills-student" role="tabpanel" aria-labelledby="pills-student-tab">
+    <div class="content">13
+    </div>
+  </div>
+
+  <div class="tab-pane fade" id="pills-speech" role="tabpanel" aria-labelledby="pills-speech-tab">
+    <div class="content">14
+    </div>
+  </div>
+
+  <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
+    <div class="content">15
+    </div>
+  </div>
+
+  <div class="tab-pane fade" id="pills-chancycle" role="tabpanel" aria-labelledby="pills-chancycle-tab">
+    <div class="content">16
+    </div>
+  </div>
+
+</div>
+
+</body>
+
+</html>
+
